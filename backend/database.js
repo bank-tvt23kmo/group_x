6faefc7e5a49c8520040c539394c1501547df8ce
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2023-11-08 17:02:16 +0200

    database environmentissa

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,12 @@
 const mysql = require('mysql2');
+const dotenv=require('dotenv');
+dotenv.config();
+const connection=mysql.createPool(process.env.SQL_SERVER);
+/*
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'peppiuser',
   password: 'peppipass',
   database: 'peppi'
-});
+});*/
 module.exports = connection;
